2642d4889940ea53742e911311ef13710b56e3a2
Nicolas Gallino <nicogf20@hotmail.com>
2025-02-28 23:24:32 -0300

    astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Para generar un sitio estático
+  output: "static",
+
+  build: {
+    assetsPrefix: "/", // Asegura rutas absolutas
+  },
+
+  base: "/astro.corregi.me",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
